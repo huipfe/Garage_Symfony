f7f983b04aa1707b8577062a19c168ab8f24a98f
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-07-26 18:41:51 +0200

    1e commit, downgrade in 5.4 stable version

--- public/index.php
+++ public/index.php
@@ -7,3 +7,25 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/node_modules/bootstrap">
+    link
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
