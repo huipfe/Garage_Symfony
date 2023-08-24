2da9a121e202d32b9b4d265b9803adf3428ddee4
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-08-24 19:34:13 +0200

    22 commit Fix list loop for, and Pagination without bundle.

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Categories;
 use App\Repository\CategoriesRepository;
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,16 +15,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoriesController extends AbstractController
 {
     #[Route('/{slug}', name: 'list')]
-    public function details($slug, EntityManagerInterface $entityManager): Response
+    public function details(
+        string $slug,
+        CategoriesRepository $categoriesRepository,
+        ProductsRepository $productsRepository,
+        Request $request
+    ): Response
     {
-        $category = $entityManager->getRepository(Categories::class)->findOneBy(['slug' => $slug]);
+        $category = $categoriesRepository->findOneBy(['slug' => $slug]);
 
         if (!$category) {
             throw $this->createNotFoundException('Product not found');
         }
+        // On va chercher le numéro de page dans l'URL
+        $page = $request->query->getInt('page', 1);
 
         #On va chercher la liste des produits de la catégorie.
-        $products = $category->getProducts();
+        $products = $productsRepository->findProductsPaginated($page, $category->getSlug(), 2);
 
         return $this->render('categories/list.html.twig' , [
             'category' => $category,
--- src/Repository/ProductsRepository.php
+++ src/Repository/ProductsRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Products;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -19,6 +20,53 @@ class ProductsRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Products::class);
+    }
+
+    public function findProductsPaginated(int $page, string $slug, int $limit = 6) : array
+    {
+
+        $limit = abs($limit); // Avoir toujours une limite positive
+
+        $result = [];
+
+
+        // La requête - chercher les catégories et les produits, et filtrer sur le slug
+        // c = catégories, p = produits, select = sélection, from = d'ou/localisation, join = jointure, where = vers Ou/localisation
+        $query = $this->getEntityManager()->createQueryBuilder()
+            ->select('c', 'p') // Selectionner tout ce qui a dans catégorie et produit
+            ->from('App\Entity\Products', 'p') // de la table products
+            ->join('p.categories', 'c') // Jointe à catégorie/par l'intermédiaire du champ catégories
+            ->where("c.slug = '$slug'") // Et on va lui dire : "tu me prends les produits qui ont commme slug de catégorie, le slug qu'on lui passe dans la fonction"
+            ->setMaxResults($limit) //Le maxmimun de résultat trouvable sur une page, sinon on pagine.
+            ->setFirstResult(($page * $limit) - $limit); // Le premier de la page dans laquelle je me trouve, page multiplié par la limite, moins la limite
+
+        //dd($query->getQuery()->getResult());
+
+        // Pagination, on veux paginer (via doctrine)
+        $paginator = new Paginator($query);
+        $data = $paginator->getQuery()->getResult();
+
+        //dd($data);
+
+        // On vérifie qu'on a des données
+        if(empty($data)){
+            return $result; // Si on a pas de donnée, on retourne le résultat
+        }
+
+        // On vérifie le nombre de page
+        $pages = ceil($paginator->count() / $limit); //Le plafond de page, via le paginator, diviser par la limite
+
+        // On va remplir le tableau
+        $result['data'] = $data; // Les données
+        $result['pages'] = $pages; // Toutes les pages
+        $result['page'] = $page; // Une page, pour savoir à quelle page je me trouve
+        $result['limit'] = $limit; //On passe notre limite
+
+
+
+
+        return $result;
+
     }
 
 //    /**
